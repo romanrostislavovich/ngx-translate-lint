ff6006bff446ab62e8d9075d5dfa15ed7fc0aafb
Roman <romanrostislavovich@gmail.com>
2025-10-02 15:11:52 +0300

    fix: fix tests for async runing linter

--- src/core/utils/http.ts
+++ src/core/utils/http.ts
@@ -3,9 +3,7 @@ export class Http {
     public static async get(url: string): Promise<any> {
         return await fetch(url).then(res => res.json()).then(res => {
             const space: number  = 4;
-           const result: string =  JSON.stringify(res, null, space);
-           console.log(result);
-           return result;
+            return  JSON.stringify(res, null, space);
         });
     }
 }
